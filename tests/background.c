c22fafbcccfd015508185815228ee42fd10085f0
marika <marika@waifu.club>
2018-05-28 19:54:27 -0300

    Add background and window tests.

--- tests/background.c
+++ tests/background.c
@@ -1,10 +1,158 @@
 #include <stdio.h>
-#include <game.h>
+#include <glboy/game.h>
+#include <glboy/image.h>
+#include <glboy/input.h>
+
+int gActiveBg;
+
+void updateMain()
+{
+    GBInputState state = gbGetInputState();
+
+    if (state.a.pressed && !state.a.repeat)
+    {
+        gActiveBg = !gActiveBg;
+        gbSetActiveBackground(gActiveBg);
+    }
+
+    int x = 0;
+    int y = 0;
+
+    if (state.up.pressed)
+        y = 1;
+    else if (state.down.pressed)
+        y = -1;
+
+    if (state.left.pressed)
+        x = 1;
+    else if (state.right.pressed)
+        x = -1;
+
+    gbGetBackground(gActiveBg)->x += x;
+    gbGetBackground(gActiveBg)->y += y;
+}
 
 int main(int argc, char *argv[])
 {
     if (!gbInit())
         return 1;
+
+    unsigned char empty[] = {
+        0x00, 0x00, 0x7E, 0x7E, 0x62, 0x46, 0x42, 0x42, 0x42, 0x42, 0x46, 0x62, 0x7E, 0x7E, 0x00, 0x00
+    };
+
+    unsigned char filled[] = {
+        0x00, 0x00, 0x7E, 0x7E, 0x42, 0x7E, 0x5A, 0x66, 0x5A, 0x66, 0x42, 0x7E, 0x7E, 0x7E, 0x00, 0x00
+    };
+
+    GLuint emptyPixels[TILE_SIZE];
+    GLuint filledPixels[TILE_SIZE];
+
+    int e = 1, f = 2;
+
+    gbLoadImage(empty, emptyPixels);
+    gbLoadImage(filled, filledPixels);
+
+    if (!gbSetTileData(TILE_DATA_BG, e, emptyPixels) ||
+        !gbSetTileData(TILE_DATA_BG, f, filledPixels))
+        return 1;
+
+    SDL_Color colours[PAL_LENGTH] = {
+        { 222, 248, 210 },
+        { 130, 192, 117 },
+        { 43, 104, 86 },
+        { 5, 24, 32 },
+    };
+
+    int palette[PAL_LENGTH] = {
+        PAL_WHITE,
+        PAL_LGREY,
+        PAL_DGREY,
+        PAL_BLACK,
+    };
+
+    gbSetColours(colours);
+    gbSetBackgroundPalette(palette);
+    gbSetUpdateCallback(updateMain);
+
+    int testBgOne[BG_HEIGHT][BG_WIDTH] = {
+        { f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f },
+        { f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, f },
+        { f, 0, f, 0, 0, f, 0, f, f, f, f, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, 0, f, f, 0, 0, 0, f, 0, 0, f },
+        { f, 0, f, 0, 0, f, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, f, 0, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, 0, f, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, f, 0, 0, f, 0, 0, 0, f, 0, f },
+        { f, 0, f, f, f, f, 0, f, f, f, 0, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, f, 0, 0, f, 0, 0, 0, f, 0, f },
+        { f, 0, f, 0, 0, f, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, f, 0, 0, f, 0, 0, f, 0, 0, f },
+        { f, 0, f, 0, 0, f, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, f, 0, 0, 0, 0, f, 0, 0, f, 0, 0, 0, 0, 0, f },
+        { f, 0, f, 0, 0, f, 0, f, f, f, f, 0, f, f, f, f, 0, f, f, f, f, 0, 0, f, f, 0, 0, 0, e, 0, 0, f },
+        { f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, f },
+        { f, 0, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, 0, f },
+        { f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, f },
+        { f, 0, f, 0, 0, f, 0, 0, f, f, 0, 0, f, f, f, 0, 0, f, 0, 0, 0, 0, f, f, f, 0, 0, 0, f, 0, 0, f },
+        { f, 0, f, 0, 0, f, 0, f, 0, 0, f, 0, f, 0, 0, f, 0, f, 0, 0, 0, 0, f, 0, 0, f, 0, 0, f, 0, 0, f },
+        { f, 0, f, 0, 0, f, 0, f, 0, 0, f, 0, f, 0, 0, f, 0, f, 0, 0, 0, 0, f, 0, 0, f, 0, 0, f, 0, 0, f },
+        { f, 0, f, 0, 0, f, 0, f, 0, 0, f, 0, f, f, f, 0, 0, f, 0, 0, 0, 0, f, 0, 0, f, 0, 0, f, 0, 0, f },
+        { f, 0, f, 0, f, f, 0, f, 0, 0, f, 0, f, 0, 0, f, 0, f, 0, 0, 0, 0, f, 0, 0, f, 0, 0, f, 0, 0, f },
+        { f, 0, f, f, f, f, 0, f, 0, 0, f, 0, f, 0, 0, f, 0, f, 0, 0, 0, 0, f, 0, 0, f, 0, 0, 0, 0, 0, f },
+        { f, 0, f, 0, 0, f, 0, 0, f, f, 0, 0, f, 0, 0, f, 0, f, f, f, f, 0, f, f, f, 0, 0, 0, e, 0, 0, f },
+        { f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, f },
+        { f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, f },
+        { f, 0, f, e, e, e, e, e, e, e, e, e, e, e, e, e, f, e, e, e, e, e, e, e, e, e, e, e, e, e, f, f },
+        { f, 0, e, f, 0, 0, 0, 0, 0, e, 0, 0, 0, 0, 0, f, e, f, 0, 0, 0, 0, 0, e, 0, 0, 0, 0, 0, f, e, f },
+        { f, 0, e, 0, f, 0, 0, 0, 0, e, 0, 0, 0, 0, f, 0, e, 0, f, 0, 0, 0, 0, e, 0, 0, 0, 0, f, 0, e, f },
+        { f, 0, e, 0, 0, f, 0, 0, 0, e, 0, 0, 0, f, 0, 0, e, 0, 0, f, 0, 0, 0, e, 0, 0, 0, f, 0, 0, e, f },
+        { f, 0, e, 0, 0, 0, f, 0, 0, e, 0, 0, f, 0, 0, 0, e, 0, 0, 0, f, 0, 0, e, 0, 0, f, 0, 0, 0, e, f },
+        { f, 0, e, 0, 0, 0, 0, f, 0, e, 0, f, 0, 0, 0, 0, e, 0, 0, 0, 0, f, 0, e, 0, f, 0, 0, 0, 0, e, f },
+        { f, 0, e, 0, 0, 0, 0, 0, f, e, f, 0, 0, 0, 0, 0, e, 0, 0, 0, 0, 0, f, e, f, 0, 0, 0, 0, 0, e, f },
+        { f, 0, e, e, e, e, e, e, e, f, e, e, e, e, e, e, e, e, e, e, e, e, e, f, e, e, e, e, e, e, e, f },
+        { f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, f },
+        { f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, f },
+        { f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f },
+    };
+
+    int testBgTwo[BG_HEIGHT][BG_WIDTH] = {
+        { f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, f },
+        { f, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f, 0, f },
+        { f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f },
+    };
+
+    for (int y = 0; y < BG_HEIGHT; y++)
+    {
+        for (int x = 0; x < BG_WIDTH; x++)
+        {
+            gbSetTileMapTile(gbGetBackground(0), x, y, testBgOne[y][x]);
+            gbSetTileMapTile(gbGetBackground(1), x, y, testBgTwo[y][x]);
+        }
+    }
 
     if (!gbRun())
         return 1;
---
+++ tests/window.c
@@ -0,0 +1,195 @@
+#include <glboy/game.h>
+#include <glboy/image.h>
+#include <glboy/input.h>
+
+int gActiveWin;
+
+void updateMain()
+{
+    GBInputState state = gbGetInputState();
+
+    if (state.a.pressed && !state.a.repeat)
+    {
+        gActiveWin = !gActiveWin;
+        gbSetActiveWindow(gActiveWin);
+    }
+
+    int x = 0;
+    int y = 0;
+
+    if (state.up.pressed)
+        y = 1;
+    else if (state.down.pressed)
+        y = -1;
+
+    if (state.left.pressed)
+        x = 1;
+    else if (state.right.pressed)
+        x = -1;
+
+    gbGetWindow(gActiveWin)->x += x;
+    gbGetWindow(gActiveWin)->y += y;
+}
+
+int main(int argc, char *argv[])
+{
+    if (!gbInit())
+        return 1;
+
+    unsigned char topLeft[] = {
+        0x00, 0x00, 0x3F, 0x3F, 0x6F, 0x70, 0x5F, 0x60, 0x7F, 0x40, 0x7F, 0x40, 0x7F, 0x40, 0x7F, 0x40
+    };
+
+    unsigned char topRight[] = {
+        0x00, 0x00, 0xFC, 0xFC, 0xF6, 0x0E, 0xFA, 0x06, 0xFE, 0x02, 0xFE, 0x02, 0xFE, 0x02, 0xFE, 0x02
+    };
+
+    unsigned char bottomLeft[] = {
+        0x7F, 0x40, 0x7F, 0x40, 0x7F, 0x40, 0x7F, 0x40, 0x5F, 0x60, 0x6F, 0x70, 0x3F, 0x3F, 0x00, 0x00
+    };
+
+    unsigned char bottomRight[] = {
+        0xFE, 0x02, 0xFE, 0x02, 0xFE, 0x02, 0xFE, 0x02, 0xFA, 0x06, 0xF6, 0x0E, 0xFC, 0xFC, 0x00, 0x00
+    };
+
+    unsigned char top[] = {
+        0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00
+    };
+
+    unsigned char bottom[] = {
+        0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0xFF, 0x00, 0x00
+    };
+
+    unsigned char left[] = {
+        0x7F, 0x40, 0x7F, 0x40, 0x7F, 0x40, 0x7F, 0x40, 0x7F, 0x40, 0x7F, 0x40, 0x7F, 0x40, 0x7F, 0x40
+    };
+
+    unsigned char right[] = {
+        0xFE, 0x02, 0xFE, 0x02, 0xFE, 0x02, 0xFE, 0x02, 0xFE, 0x02, 0xFE, 0x02, 0xFE, 0x02, 0xFE, 0x02
+    };
+
+    unsigned char text[] = {
+        0xFF, 0x00, 0x28, 0xD7, 0xFF, 0x00, 0x14, 0xEB, 0xFF, 0x00, 0x21, 0xDE, 0xFF, 0x00, 0x80, 0x7F
+    };
+
+    GLuint topLeftPixels[TILE_SIZE];
+    GLuint topRightPixels[TILE_SIZE];
+    GLuint bottomLeftPixels[TILE_SIZE];
+    GLuint bottomRightPixels[TILE_SIZE];
+    GLuint topPixels[TILE_SIZE];
+    GLuint bottomPixels[TILE_SIZE];
+    GLuint leftPixels[TILE_SIZE];
+    GLuint rightPixels[TILE_SIZE];
+    GLuint textPixels[TILE_SIZE];
+
+    gbLoadImage(topLeft, topLeftPixels);
+    gbLoadImage(topRight, topRightPixels);
+    gbLoadImage(bottomLeft, bottomLeftPixels);
+    gbLoadImage(bottomRight, bottomRightPixels);
+    gbLoadImage(top, topPixels);
+    gbLoadImage(bottom, bottomPixels);
+    gbLoadImage(left, leftPixels);
+    gbLoadImage(right, rightPixels);
+    gbLoadImage(text, textPixels);
+
+    int tl = 1, tr = 2, bl = 3, br = 4, t = 5, b = 6, l = 7, r = 8, x = 9;
+
+    if (!gbSetTileData(TILE_DATA_BG, tl, topLeftPixels) ||
+        !gbSetTileData(TILE_DATA_BG, tr, topRightPixels) ||
+        !gbSetTileData(TILE_DATA_BG, bl, bottomLeftPixels) ||
+        !gbSetTileData(TILE_DATA_BG, br, bottomRightPixels) ||
+        !gbSetTileData(TILE_DATA_BG, t, topPixels) ||
+        !gbSetTileData(TILE_DATA_BG, b, bottomPixels) ||
+        !gbSetTileData(TILE_DATA_BG, l, leftPixels) ||
+        !gbSetTileData(TILE_DATA_BG, r, rightPixels) ||
+        !gbSetTileData(TILE_DATA_BG, x, textPixels))
+        return 1;
+
+    SDL_Color colours[PAL_LENGTH] = {
+        { 222, 248, 210 },
+        { 130, 192, 117 },
+        { 43, 104, 86 },
+        { 5, 24, 32 },
+    };
+
+    int palette[PAL_LENGTH] = {
+        PAL_WHITE,
+        PAL_LGREY,
+        PAL_DGREY,
+        PAL_BLACK,
+    };
+
+    gbSetColours(colours);
+    gbSetBackgroundPalette(palette);
+    gbSetUpdateCallback(updateMain);
+
+    // window one
+    const int testWinOneWidth = TILES_X;
+    const int testWinOneHeight = 5;
+
+    gbGetWindow(0)->y = (TILES_Y - testWinOneHeight) * TILE_HEIGHT;
+    gbGetWindow(0)->width = testWinOneWidth;
+    gbGetWindow(0)->height = testWinOneHeight;
+
+    int *testWinOneTiles = (int *)calloc(testWinOneWidth * testWinOneHeight, sizeof(int));
+    gbGetWindow(0)->tiles = testWinOneTiles;
+
+    int testWinOne[testWinOneHeight][testWinOneWidth] = {
+        { tl, t, t, t, t, t, t, t, t, t, t, t, t, t, t, t, t, t, t, tr },
+        { l, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, r },
+        { l, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, r },
+        { l, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, r },
+        { bl, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, br },
+    };
+
+    for (int y = 0; y < testWinOneHeight; y++)
+    {
+        for (int x = 0; x < testWinOneWidth; x++)
+        {
+            gbSetTileMapTile(gbGetWindow(0), x, y, testWinOne[y][x]);
+        }
+    }
+
+    // window two
+    const int testWinTwoWidth = 5;
+    const int testWinTwoHeight = 5;
+
+    gbGetWindow(1)->width = testWinTwoWidth;
+    gbGetWindow(1)->height = testWinTwoHeight;
+
+    int *testWinTwoTiles = (int *)calloc(testWinTwoWidth * testWinTwoHeight, sizeof(int));
+    gbGetWindow(1)->tiles = testWinTwoTiles;
+
+    int testWinTwo[testWinTwoHeight][testWinTwoWidth] = {
+        { tl, t, t, t, tr },
+        { l, x, x, x, r },
+        { l, x, x, x, r },
+        { l, x, x, x, r },
+        { bl, b, b, b, br },
+    };
+
+    for (int y = 0; y < testWinTwoHeight; y++)
+    {
+        for (int x = 0; x < testWinTwoWidth; x++)
+        {
+            gbSetTileMapTile(gbGetWindow(1), x, y, testWinTwo[y][x]);
+        }
+    }
+
+    if (!gbRun())
+        return 1;
+
+    if (!gbQuit())
+        return 1;
+
+    free(testWinOneTiles);
+    free(testWinTwoTiles);
+
+    if (!gbRun())
+        return 1;
+
+    if (!gbQuit())
+        return 1;
+
+    return 0;
+}
